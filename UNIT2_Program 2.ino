87c8c874da71512b61c3f681e03627d11b4f4166
anop <81241987+mhmdnvn18@users.noreply.github.com>
2023-11-08 23:12:42 +0700

    UNIT2_Program 2.ino

---
+++ UNIT2_Program 2.ino
@@ -0,0 +1,35 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+const int SW1 = 12;
+const int SW2 = 13;
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  pinMode(SW1, INPUT);
+  pinMode(SW2, INPUT);
+
+  lcd.init();
+}
+
+void loop() {
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Tombol");
+
+  if (digitalRead(SW1) == HIGH) {
+    lcd.setCursor(0, 1);
+    lcd.print("tombol 1");
+  }
+
+  delay(500);
+  lcd.clear();
+
+  if (digitalRead(SW2) == HIGH) {
+    lcd.setCursor(0, 1);
+    lcd.print("tombol 2");
+    delay(500);
+    lcd.clear();
+  }
+}
