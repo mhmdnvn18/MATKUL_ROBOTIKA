27afc27d27d011328e64d92eec075f08319a14ab
anop <81241987+mhmdnvn18@users.noreply.github.com>
2023-11-09 06:21:18 +0700

    Update UNIT2_Program 2.ino

--- UNIT2_Program 2.ino
+++ UNIT2_Program 2.ino
@@ -6,27 +6,29 @@ const int SW2 = 13;
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-void setup() {
+void setup() 
+{
   pinMode(SW1, INPUT);
   pinMode(SW2, INPUT);
-
   lcd.init();
 }
 
-void loop() {
+void loop() 
+{
   lcd.backlight();
   lcd.setCursor(0, 0);
   lcd.print("Tombol");
 
-  if (digitalRead(SW1) == HIGH) {
+  if (digitalRead(SW1) == HIGH) 
+  {
     lcd.setCursor(0, 1);
     lcd.print("tombol 1");
+    delay(500);
+    lcd.clear();
   }
 
-  delay(500);
-  lcd.clear();
-
-  if (digitalRead(SW2) == HIGH) {
+  if (digitalRead(SW2) == HIGH) 
+  {
     lcd.setCursor(0, 1);
     lcd.print("tombol 2");
     delay(500);
